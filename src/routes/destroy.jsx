457d00e91d8180a39718751e585674ac095ff824
Bayu Maulana <bayufransdo@gmail.com>
2023-12-24 19:53:05 +0700

    CRUD done, search feature done

---
+++ src/routes/destroy.jsx
@@ -0,0 +1,12 @@
+import { redirect } from 'react-router-dom';
+import { deleteContact } from '../contacts';
+
+export async function action({ params }) {
+  await deleteContact(params.contactId);
+  return redirect(`/`);
+}
+
+const Destroy = () => {
+  return <div>Destroy</div>;
+};
+export default Destroy;
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,24 +1,35 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from 'react-router-dom';
+import {
+  Outlet,
+  Link,
+  useLoaderData,
+  Form,
+  redirect,
+  NavLink,
+  useNavigation,
+} from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
 export async function action() {
   const contact = await createContact();
   return redirect(`/contacts/${contact.id}/edit`);
 }
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
 const Root = () => {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
@@ -28,7 +39,7 @@ const Root = () => {
             />
             <div className="search-spinner" aria-hidden hidden={true}></div>
             <div className="sr-only" aria-live="polite"></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
@@ -38,7 +49,12 @@ const Root = () => {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -47,7 +63,7 @@ const Root = () => {
                       <i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -58,7 +74,10 @@ const Root = () => {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
